7b51e66196d92f8f53b04690bef82dab79739df7
Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2023-02-06 17:58:45 -0500

    fix sqlite url

--- src/modelo/declarative_base.py
+++ src/modelo/declarative_base.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('sqlite://baseDeDatos.sqlite')
+engine = create_engine('sqlite:///baseDeDatos.sqlite')
 Session = sessionmaker(bind=engine)
 
 Base = declarative_base()
