62d930c03853d4c8e80b9050d142e0da6e66f537
Esneider Velandia Gonzalez <e.velandia2164@uniandes.edu.co>
2023-02-06 13:59:44 -0500

    add update use case

---
+++ src/useCase/actualizar.py
@@ -0,0 +1,18 @@
+from modelo.cancion import Cancion
+from modelo.declarative_base import Session
+from modelo.interprete import Interprete
+
+if __name__ == '__main__':
+  session = Session()
+  cancion = session.query(Cancion).get(2)
+  interprete = session.query(Interprete).get(4)
+
+  cancion.minutos = 5
+  cancion.segundos = 30
+  cancion.compositor = "Pedro Pérez"
+  cancion.interpretes.append(interprete)
+  
+  session.add(cancion)
+
+  session.commit()
+  session.close()
